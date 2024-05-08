c502744fe1b5ac49d0a6b51c061817528fa8dd6d
Anton Hirov <hirov.anton@yahoo.com>
2024-05-08 18:02:45 +0300

    Added data project

---
+++ Data/Class1.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="Class1.cs" company="Anton Hirov - Private entrepreneur">
+// Copyright (c) Anton Hirov - Private entrepreneur. All rights reserved.
+// </copyright>
+
+namespace ElectronicLedger.Data
+{
+    public class Class1
+    {
+
+    }
+}
