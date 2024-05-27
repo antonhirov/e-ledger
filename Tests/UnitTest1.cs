8c7b1efd22c1d30e92151d983e9733df5aa62ed9
Anton Hirov <hirov.anton@yahoo.com>
2024-05-27 13:22:25 +0300

    Added Api test project

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ElectronicLedger.Api.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
